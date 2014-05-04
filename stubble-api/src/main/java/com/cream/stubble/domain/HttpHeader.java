5be8492afbfc0b1332e32919c08eb77d6f3dcfb9
dmacdonald2013 <d.macdonald@auckland.ac.nz>
2014-05-04 19:56:31 +1200

    added support for returning JSON and XML

--- stubble-api/src/main/java/com/cream/stubble/domain/HttpHeader.java
+++ stubble-api/src/main/java/com/cream/stubble/domain/HttpHeader.java
@@ -1,11 +1,34 @@
 package com.cream.stubble.domain;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * A simple string->string value for headers
  *
  * @author David MacDonald
  */
+@XmlRootElement(name="header")
 public class HttpHeader {
     private String name;
     private String value;
+
+    HttpHeader() {
+        //jaxb wants this
+    }
+
+    public HttpHeader(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    @XmlElement(name="name")
+    public String getName() {
+        return this.name;
+    }
+
+    @XmlElement(name="value")
+    public String getValue() {
+        return this.value;
+    }
 }
--- stubble-api/src/main/java/com/cream/stubble/service/EndpointController.java
+++ stubble-api/src/main/java/com/cream/stubble/service/EndpointController.java
@@ -1,12 +1,22 @@
 package com.cream.stubble.service;
 
+import com.cream.stubble.domain.HttpHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/endpoints")
 public class EndpointController {
-    @RequestMapping("/")
-    public String index() {
-        return "foo";
+
+    //can't return array with XML
+    @RequestMapping(method=RequestMethod.GET)
+    public List<HttpHeader> getAll() {
+        List<HttpHeader> headers = new ArrayList<>();
+        headers.add(new HttpHeader("foo","baz"));
+        return headers;
     }
 }

3c18a814ec6a74266db8cf14a43dae209c5bcb31
dmacdonald2013 <d.macdonald@auckland.ac.nz>
2014-05-05 21:25:36 +1200

    added xml serialization

--- stubble-api/src/main/java/com/cream/stubble/domain/HttpEndpoint.java
+++ stubble-api/src/main/java/com/cream/stubble/domain/HttpEndpoint.java
@@ -1,5 +1,8 @@
 package com.cream.stubble.domain;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,8 +10,37 @@ import java.util.List;
  *
  * @author David MacDonald
  */
+@XmlRootElement(name="endpoint")
 public class HttpEndpoint {
     private String endpointUri;
     private String description;
     private List<HttpOutput> outputs;
+
+    @XmlElement(name="description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @XmlElement(name="outputs")
+    public List<HttpOutput> getOutputs() {
+        return Collections.unmodifiableList(outputs);
+    }
+
+    public void setOutputs(List<HttpOutput> outputs) {
+        this.outputs = outputs;
+    }
+
+    @XmlElement(name="endpointUri")
+    public String getEndpointUri() {
+        return endpointUri;
+    }
+
+    public void setEndpointUri(String endpointUri) {
+        this.endpointUri = endpointUri;
+    }
+
 }
--- stubble-api/src/main/java/com/cream/stubble/domain/HttpOutput.java
+++ stubble-api/src/main/java/com/cream/stubble/domain/HttpOutput.java
@@ -1,5 +1,9 @@
 package com.cream.stubble.domain;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -7,8 +11,41 @@ import java.util.List;
  *
  * @author David MacDonald
  */
-public class HttpOutput {
+@XmlRootElement(name="output")
+public class HttpOutput implements Serializable {
+
     private int responseCode = 200;
     private String body;
     private List<HttpHeader> headers;
+
+    public HttpOutput() {
+
+    }
+
+    @XmlElement(name="responseCode")
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    @XmlElement(name="body")
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @XmlElement(name="headers")
+    public List<HttpHeader> getHeaders() {
+        return Collections.unmodifiableList(headers);
+    }
+
+    public void setHeaders(List<HttpHeader> headers) {
+        this.headers = headers;
+    }
 }

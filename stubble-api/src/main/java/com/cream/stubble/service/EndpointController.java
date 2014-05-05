349067486737be150f513c9f8ba72b2ce1377afc
dmacdonald2013 <davidm@cdonald.net.nz>
2014-05-05 12:56:25 +1200

    sorted wrapping for XML content-type

--- stubble-api/src/main/java/com/cream/stubble/service/EndpointController.java
+++ stubble-api/src/main/java/com/cream/stubble/service/EndpointController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.bind.annotation.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,4 +21,30 @@ public class EndpointController {
         headers.add(new HttpHeader("foo","baz"));
         return headers;
     }
+
+    @RequestMapping(method=RequestMethod.GET,produces={"application/xml"})
+    public HttpHeaders getAllXml() {
+        //we need to wrap up XML
+        return new HttpHeaders(getAll());
+    }
+}
+
+@XmlRootElement(name="headers")
+@XmlSeeAlso({HttpHeader.class})
+class HttpHeaders implements Serializable {
+    private List<HttpHeader> headers;
+
+    HttpHeaders() {
+        //for Jaxb
+    }
+
+    public HttpHeaders(List<HttpHeader> headers) {
+        this.headers = headers;
+    }
+
+    @XmlAnyElement(lax=true)
+    public List<HttpHeader> getHeaders() {
+        return headers;
+    }
+
 }
